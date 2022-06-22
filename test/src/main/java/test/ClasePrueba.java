c007e69794cdaa2199295d17672bf270cfd332c4
raulr <raulr@LAPTOP-JSHKNSET>
2022-06-22 18:00:42 +0200

    Subimos antes de la pull request

--- test/src/main/java/test/ClasePrueba.java
+++ test/src/main/java/test/ClasePrueba.java
@@ -1,12 +1,12 @@
 package test;
 
 public class ClasePrueba {
-	
-	//Prueba
-	//Comentario
-	
-	//Comentario añadido desde GITHUB!!
-	
-	//Otro comentario
+
+	// Prueba
+	// Comentario
+
+	// Comentario añadido desde GITHUB!!
+
+	// Quiero subir esto a la rama MASTER
 
 }
